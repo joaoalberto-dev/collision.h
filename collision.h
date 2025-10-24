18819a8668ee3bf967014b63364332be7507e713
João Alberto <hey@joaoalberto.dev>
2025-10-24 16:26:25 -0300

    chore: remove c++ stuff

--- collision.h
+++ collision.h
@@ -1,10 +1,7 @@
 #ifndef COLLISION_H
 #define COLLISION_H
-#ifdef __cpluplus
-extern "C"
-#endif
 
-    int point_point(int p1_x, int p1_y, int p2_x, int p2_y);
+int point_point(int p1_x, int p1_y, int p2_x, int p2_y);
 
 #ifdef COLLISION_IMPLEMENTATION
 
@@ -13,8 +10,5 @@ int point_point(int p1_x, int p1_y, int p2_x, int p2_y)
     return p1_x == p2_x && p1_y == p2_y;
 }
 
-#endif
-
-#ifdef __cplusplus
 #endif
 #endif
