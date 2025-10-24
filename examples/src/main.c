421947363ceb830b46c5c618f31912a7328d0363
João Alberto <hey@joaoalberto.dev>
2025-10-24 20:52:36 -0300

    chore: adding point_point example

--- examples/src/main.c
+++ examples/src/main.c
@@ -1,17 +1,39 @@
+#include <stdio.h>
 #include "raylib.h"
+
+#define COLLISION_IMPLEMENTATION
+#include "../../collision.h"
+
+const int DPI = 2;
+
+static inline Point toPoint(struct Vector2 vec)
+{
+	return (Point){vec.x, vec.y};
+}
 
 int main()
 {
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
 	InitWindow(1280, 800, "Collision");
+	HideCursor();
 
 	while (!WindowShouldClose())
 	{
+
+		float centerX = GetRenderWidth() / 2 / DPI;
+		float centerY = GetRenderHeight() / 2 / DPI;
+
+		Vector2 mouse = GetMousePosition();
+		int is_colliding = point_point(toPoint(mouse), (Point){.x = centerX, .y = centerY});
+
 		BeginDrawing();
 		ClearBackground(BLACK);
-		DrawText("Collision", 0, 0, 20, WHITE);
+		DrawCircle(centerX, centerY, 10, RED);
+		DrawCircle(mouse.x, mouse.y, 10, is_colliding ? BLUE : RED);
 		EndDrawing();
 	}
+
+	ShowCursor();
 	CloseWindow();
 	return 0;
 }
