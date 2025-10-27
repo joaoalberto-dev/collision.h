d25eb9c786ef99a3e3d16ead5eb5910060bcc0aa
João Alberto <hey@joaoalberto.dev>
2025-10-27 20:24:12 -0300

    chore: rectangle rectangle collision example

--- examples/src/main.c
+++ examples/src/main.c
@@ -20,19 +20,25 @@ int main()
 	while (!WindowShouldClose())
 	{
 
+		Vector2 mouse = GetMousePosition();
+
 		float center_x = GetRenderWidth() / 2 / DPI;
 		float center_y = GetRenderHeight() / 2 / DPI;
-		Vector2 mouse = GetMousePosition();
-		int rx = center_x - 50;
-		int ry = center_y - 50;
-		int rw = 100;
-		int rh = 100;
-		int is_colliding = point_rectangle(to_point(mouse), rx, ry, rw, rh);
+
+		int r1w = 10;
+		int r1h = 10;
+
+		int r2x = center_x - 50;
+		int r2y = center_y - 50;
+		int r2w = 100;
+		int r2h = 100;
+
+		int is_colliding = rectangle_rectangle(mouse.x, mouse.y, r1w, r1h, r2x, r2y, r2w, r2h);
 
 		BeginDrawing();
 		ClearBackground(BLACK);
-		DrawRectangle(rx, ry, rw, rh, is_colliding ? BLUE : RED);
-		DrawCircle(mouse.x, mouse.y, 1, RED);
+		DrawRectangle(r2x, r2y, r2w, r2h, is_colliding ? BLUE : RED);
+		DrawRectangle(mouse.x, mouse.y, r1w, r1h, RED);
 		EndDrawing();
 	}
 
