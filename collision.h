caf96fd600ca597da8898e428c2be6987f445857
João Alberto <hey@joaoalberto.dev>
2025-10-27 19:33:49 -0300

    chore: point circle collision

--- collision.h
+++ collision.h
@@ -1,3 +1,5 @@
+#include <math.h>
+
 #ifndef COLLISION_H
 #define COLLISION_H
 
@@ -8,12 +10,22 @@ typedef struct Point
 } Point;
 
 int point_point(Point p1, Point p2);
+int point_circle(Point p, Point c, float r);
 
 #ifdef COLLISION_IMPLEMENTATION
 
 int point_point(Point p1, Point p2)
 {
     return p1.x == p2.x && p1.y == p2.y;
+}
+
+int point_circle(Point p, Point c, float r)
+{
+    float distX = p.x - c.x;
+    float distY = p.y - c.y;
+    float distance = sqrt((distX * distX) + (distY * distY));
+
+    return r > distance;
 }
 
 #endif
--- test.c
+++ test.c
@@ -14,8 +14,25 @@ void test_point_point(void)
     printf("✓ test_point_point passed\n");
 }
 
+void test_point_circle(void)
+{
+    Point p1 = {.x = 10, .y = 10};
+    Point c1 = {.x = 100, .y = 100};
+    int r1 = 10;
+
+    Point p2 = {.x = 10, .y = 10};
+    Point c2 = {.x = 15, .y = 15};
+    int r2 = 20;
+
+    assert(point_circle(p1, c1, r1) == 0);
+    assert(point_circle(p2, c2, r2) == 1);
+
+    printf("✓ test_point_circle passed\n");
+}
+
 int main(void)
 {
     test_point_point();
+    test_point_circle();
     printf("✓ All tests passed\n");
 }
