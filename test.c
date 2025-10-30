69855abe2abf56c9a3927dfd80456c9a83e2a3d4
João Alberto <hey@joaoalberto.dev>
2025-10-30 16:32:06 -0300

    chore: sort tests

--- test.c
+++ test.c
@@ -3,15 +3,25 @@
 #include <stdio.h>
 #include <assert.h>
 
-void test_point_point(void)
+void test_circle_circle(void)
 {
-    Point p1 = {.x = 1, .y = 1};
-    Point p2 = {.x = 2, .y = 2};
+    Point c1 = {.x = 10, .y = 10};
+    Point c2 = {.x = 50, .y = 50};
+    Point c3 = {.x = 20, .y = 20};
 
-    assert(point_point(p1, p1) == 1);
-    assert(point_point(p1, p2) == 0);
+    assert(circle_circle(c1, 10, c2, 10) == 0);
+    assert(circle_circle(c1, 10, c3, 10) == 1);
 
-    printf("✓ test_point_point passed\n");
+    printf("✓ test_circle_circle passed\n");
+}
+
+void test_circle_rectangle(void)
+{
+
+    assert(circle_rectangle(10, 10, 10, 50, 50, 10, 10) == 0);
+    assert(circle_rectangle(10, 10, 10, 15, 15, 10, 10) == 1);
+
+    printf("✓ test_circle_rectangle passed\n");
 }
 
 void test_point_circle(void)
@@ -30,16 +40,28 @@ void test_point_circle(void)
     printf("✓ test_point_circle passed\n");
 }
 
-void test_circle_circle(void)
+void test_point_line(void)
 {
-    Point c1 = {.x = 10, .y = 10};
-    Point c2 = {.x = 50, .y = 50};
-    Point c3 = {.x = 20, .y = 20};
+    Point c1 = {.x = 11, .y = 5};
+    Point c2 = {.x = 10, .y = 5};
+    Point l1 = {.x = 10, .y = 0};
+    Point l2 = {.x = 10, .y = 10};
 
-    assert(circle_circle(c1, 10, c2, 10) == 0);
-    assert(circle_circle(c1, 10, c3, 10) == 1);
+    assert(point_line(c1, l1, l2) == 0);
+    assert(point_line(c2, l1, l2) == 1);
 
-    printf("✓ test_circle_circle passed\n");
+    printf("✓ test_point_line passed\n");
+}
+
+void test_point_point(void)
+{
+    Point p1 = {.x = 1, .y = 1};
+    Point p2 = {.x = 2, .y = 2};
+
+    assert(point_point(p1, p1) == 1);
+    assert(point_point(p1, p2) == 0);
+
+    printf("✓ test_point_point passed\n");
 }
 
 void test_point_rectangle(void)
@@ -61,37 +83,15 @@ void test_rectangle_rectangle(void)
     printf("✓ test_rectangle_rectangle passed\n");
 }
 
-void test_circle_rectangle(void)
-{
-
-    assert(circle_rectangle(10, 10, 10, 50, 50, 10, 10) == 0);
-    assert(circle_rectangle(10, 10, 10, 15, 15, 10, 10) == 1);
-
-    printf("✓ test_circle_rectangle passed\n");
-}
-
-void test_point_line(void)
-{
-    Point c1 = {.x = 11, .y = 5};
-    Point c2 = {.x = 10, .y = 5};
-    Point l1 = {.x = 10, .y = 0};
-    Point l2 = {.x = 10, .y = 10};
-
-    assert(point_line(c1, l1, l2) == 0);
-    assert(point_line(c2, l1, l2) == 1);
-
-    printf("✓ test_point_line passed\n");
-}
-
 int main(void)
 {
-    test_point_point();
-    test_point_circle();
     test_circle_circle();
+    test_circle_rectangle();
+    test_point_circle();
+    test_point_line();
+    test_point_point();
     test_point_rectangle();
     test_rectangle_rectangle();
-    test_circle_rectangle();
-    test_point_line();
 
     printf("✓ All tests passed\n");
 }
