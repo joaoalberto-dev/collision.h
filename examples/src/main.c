b147e543833596b61c77bd4328e6f3f23308c98e
João Alberto <hey@joaoalberto.dev>
2025-11-04 19:37:22 -0300

    chore: line line collision example

--- examples/src/main.c
+++ examples/src/main.c
@@ -26,15 +26,17 @@ int main()
 		float center_x = GetRenderWidth() / 2 / finalScale;
 		float center_y = GetRenderHeight() / 2 / finalScale;
 
-		Vector2 l1 = {.x = center_x, .y = 100};
-		Vector2 l2 = {.x = center_x, .y = (center_y * 2) - 100};
+		Vector2 p1 = {.x = center_x, .y = 100};
+		Vector2 p2 = {.x = center_x, .y = (center_y * 2) - 100};
+		Vector2 p3 = {.x = 100, .y = center_y - 100};
+		Vector2 p4 = {.x = mouse.x, .y = mouse.y};
 
-		int is_colliding = circle_line(mouse.x, mouse.y, 10, to_point(l1), to_point(l2));
+		int is_colliding = line_line(to_point(p1), to_point(p2), to_point(p3), to_point(p4));
 
 		BeginDrawing();
 		ClearBackground(BLACK);
-		DrawLine(l1.x, l1.y, l2.x, l2.y, is_colliding ? BLUE : RED);
-		DrawCircle(mouse.x, mouse.y, 10, is_colliding ? BLUE : RED);
+		DrawLine(p1.x, p1.y, p2.x, p2.y, is_colliding ? BLUE : RED);
+		DrawLine(p3.x, p3.y, p4.x, p4.y, is_colliding ? BLUE : RED);
 		EndDrawing();
 	}
 
